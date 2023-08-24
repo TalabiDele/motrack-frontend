4bb82d5e293f84db3e0c5919b354a6e678e3485f
talabidele <talabibamidele7@gmail.com>
2023-08-24 14:47:53 +0100

    Added PWA

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import "leaflet/dist/leaflet.css";
-import { AuthProvider } from "./components/context/AuthContext";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import 'leaflet/dist/leaflet.css'
+import { AuthProvider } from './components/context/AuthContext'
+import { BrowserRouter as Router } from 'react-router-dom'
+import reportWebVitals from './reportWebVitals'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  // <React.StrictMode>
-  <Router>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </Router>
-  // </React.StrictMode>
-);
+	// <React.StrictMode>
+	<Router>
+		<AuthProvider>
+			<App />
+		</AuthProvider>
+	</Router>
+	// </React.StrictMode>
+)
+
+serviceWorkerRegistration.register()
+
+reportWebVitals()
